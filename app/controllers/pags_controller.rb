edf4ceab7da989a27b354140ffa0530ef0e7236d
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-02 19:48:34 -0500

    mostrando listado de alumons en las tres paginas

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,4 +1,6 @@
 class PagsController < ApplicationController
+  before_action :get_data
+
   def pag1
     render layout: "landing"
   end
@@ -7,5 +9,10 @@ class PagsController < ApplicationController
   end
 
   def pag3
+  end
+
+  private
+  def get_data
+     @users = ["Alumno1", "Alumno2", "Alumno3", "Alumno4", "Alumno5", "Alumno6", "Alumno7", "Alumno8", "Alumno9", "Alumno10", "Alumno11", "Alumno12", "Alumno13"]
   end
 end
